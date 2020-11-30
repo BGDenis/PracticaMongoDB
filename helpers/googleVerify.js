19d26ac79c8f771185d6424fe27ebfd98f330721
Denis Bueno <dbuenog16_1@unc.edu.pe>
2020-11-30 16:42:57 -0500

    proyecto fin actulizacion 1

---
+++ helpers/googleVerify.js
@@ -0,0 +1,19 @@
+const {
+    OAuth2Client
+} = require('google-auth-library');
+
+const client = new OAuth2Client(process.env.GOOGLE_ID);
+const googleVerify = async(token) => {
+    const tickt = await client.verifyIdToken({
+        idToken: token,
+        audience: process.env.GOOGLE_ID
+    });
+
+    const payload = tickt.getPayload();
+    const userid = payload['sub'];
+    const { name, email, picture } = payload;
+    return { name, email, picture };
+}
+module.exports = {
+    googleVerify
+}
